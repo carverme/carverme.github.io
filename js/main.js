daf7a4efb4bfdeadff7ee5038680ff9000c1740e
Matt Carver <mattcarver.wd@gmail.com>
2018-08-22 17:51:01 -0700

    updated more

--- js/main.js
+++ js/main.js
@@ -3,4 +3,13 @@ console.log("jscript working")
 
 $(document).ready(function(){
    $('.parallax').parallax();
+   //Attempt to create transparent to solid navbar on scroll.
+   $(window).scroll(function() {
+     //This function checks if window is scrolled more than 500px, add/removes solid class.
+     if($(this).scrollTop() > 500) {
+        $('.navbar').addClass('solid');
+     } else {
+        $('.navbar').removeClass('solid');
+     }
+   });
  });
