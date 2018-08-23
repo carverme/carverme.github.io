56f98bb5f51850dc3e5779a4d0a6856526658201
Matt Carver <mattcarver.wd@gmail.com>
2018-08-22 23:17:47 -0700

    updated scrollspy

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ console.log("jscript working")
 
 $(document).ready(function(){
    $('.parallax').parallax();
+   $('.scrollspy').scrollSpy();
+
+   
    //Attempt to create transparent to solid navbar on scroll.
    $(window).scroll(function() {
      //This function checks if window is scrolled more than 500px, add/removes solid class.
