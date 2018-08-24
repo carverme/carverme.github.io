51954455b29711fe550d910be732b9fbca6bffc6
Matt Carver <mattcarver.wd@gmail.com>
2018-08-23 17:56:25 -0700

    added fonts, adjusted navhide

--- js/main.js
+++ js/main.js
@@ -1,18 +1,22 @@
 console.log("jscript working")
 
 
+
 $(document).ready(function(){
    $('.parallax').parallax();
    $('.scrollspy').scrollSpy();
+   // $(window).on("scroll", function() {
+   //   var fromTop = $(window).scrollTop();
+   //   $("navbar").toggleClass("on-scroll", (fromTop > 20));
+   // });
+ });
 
-   
-   //Attempt to create transparent to solid navbar on scroll.
-   $(window).scroll(function() {
-     //This function checks if window is scrolled more than 500px, add/removes solid class.
-     if($(this).scrollTop() > 500) {
-        $('.navbar').addClass('solid');
-     } else {
-        $('.navbar').removeClass('solid');
-     }
-   });
+ var prev = 50;
+ var $window = $(window);
+ var nav = $('#nav');
+
+ $window.on('scroll', function(){
+   var scrollTop = $window.scrollTop();
+   nav.toggleClass('hidden', scrollTop < prev);
+   prev = scrollTop;
  });
